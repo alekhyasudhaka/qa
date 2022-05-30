cd9b4650575d60e7ab17ad327b15c7aebaa4541a
alekhyasudhaka <106391386+alekhyasudhaka@users.noreply.github.com>
2022-05-30 12:12:47 +0530

    Add files via upload

---
+++ demo1_test.js
@@ -0,0 +1,11 @@
+Feature(' login page');
+
+Scenario('facebook login page', ({ I }) => {
+
+  I.amOnPage('https://www.facebook.com/login/');
+ I.fillField('email','alekhya@gmail.com');
+ I.fillField('pass','password');
+ I.click('login');
+ 
+
+});
